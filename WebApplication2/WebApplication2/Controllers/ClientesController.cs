6ad916a722e8eb9e253671e7d139e56170893781
alisson-ap <alisson08silva@gmail.com>
2020-07-22 16:02:01 -0300

    meu primeiro commit

---
+++ WebApplication2/WebApplication2/Controllers/ClientesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        public static List<Cliente> _cliente; 
+        public ClientesController(){
+
+            if(_cliente == null)
+            {
+                _cliente = new List<Cliente>();
+            }
+        }
+        [HttpPost]
+
+        public IActionResult Cadastrar(Cliente nome)
+        {
+            _cliente.Add(nome);
+
+            return StatusCode(201);
+        }
+
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Cliente
+    {
+        public string Nome{ get; set; }
+ 
+        public Cliente(string nome)
+        {
+            this.Nome = nome;
+        }
+
+    }
+ 
+}
